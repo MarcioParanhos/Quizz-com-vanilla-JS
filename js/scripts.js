53d821ffa320b12d8d9cbd474efa9abe59c9b935
Marcio Paranhos <marcio.naga@gmail.com>
2022-09-02 22:16:29 -0300

    Ajustes Importantes

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,7 @@ const scoreContainer = document.querySelector("#score-container");
 const letters = ["a", "b", "c", "d"];
 let points = 0;
 let actualQuestions = 0;
+let numberNoRepeat = [];
 
 // Pega o parametro passado pela url
 var query = location.search.slice(1);
@@ -18,21 +19,38 @@ partes.forEach(function (parte) {
   data[chave] = valor;
 });
 
-// Import das questões de cada tecnologia 
+// Import das questões de cada tecnologia
 import questionsPhp from "../modules/questionsPhp.js";
 import questionsJs from "../modules/questionsJs.js";
 
 if (data.tech === "php") {
-  console.log(questionsPhp.length)
-  // PERGUNTAS  
+  console.log(questionsPhp.length);
+
+  // PERGUNTAS
   // SUBISTITUIÇÂO DO QUIZZ PARA A PRIMEIRA PERGUNTA
   function init() {
+    //Zera o array de consulta de numero aleatorio ao iniciar o jogo novamente
+    numberNoRepeat = [];
     // Criar a primeira pergunta Aleatoria
     createQuestion(getRandomInt(0, questionsPhp.length));
   }
   //Função para gerar numero aleatorio
   function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
+    const numberRandom = Math.floor(Math.random() * (max - min)) + min;
+    if (numberNoRepeat.indexOf(numberRandom) === -1) {
+      numberNoRepeat.push(numberRandom);
+      return numberRandom;
+    } else {
+      //Chama uma função pra escolher outro numero aleatorio caso o numero ja foi sorteado  
+      nextQuestionRandom();
+    }
+  }
+  function nextQuestionRandom() {
+    //Timer para usuario ver as respostas
+    setTimeout(function () {
+      //Deixar as questões Aleatorias nas proximas perguntas
+      createQuestion(getRandomInt(0, questionsPhp.length));
+    }, 0);
   }
 
   // cria uma pergunta
@@ -100,7 +118,7 @@ if (data.tech === "php") {
     //Timer para usuario ver as respostas
     setTimeout(function () {
       //Verifica se ainda há perguntas
-      if (actualQuestions >= 10) {
+      if (actualQuestions >= 15) {
         //apresenta a mensagem de sucesso
         showSuccessMessage();
         return;
@@ -115,7 +133,7 @@ if (data.tech === "php") {
     hideOrShowQuizz();
     //TROCAR DADOS DA TABELA DE SUCESSO
     //   Calcular o score
-    const score = ((points / 10) * 100).toFixed(2);
+    const score = ((points / 15) * 100).toFixed(2);
     const displayScore = document.querySelector("#display-score span");
     displayScore.textContent = score.toString();
     //Alterar o numero de perguntas corretas
@@ -123,7 +141,7 @@ if (data.tech === "php") {
     correctAnswers.textContent = points;
     //Alterar o total de perguntas
     const totalQuestions = document.querySelector("#questions-qty");
-    totalQuestions.textContent = 10;
+    totalQuestions.textContent = 15;
   }
 
   //Mostra ou esconde o score
@@ -144,125 +162,6 @@ if (data.tech === "php") {
 
   //Inicialização do Quizz
   init();
-} else if (data.tech === "js") {
-  // PERGUNTAS  
-  // SUBISTITUIÇÂO DO QUIZZ PARA A PRIMEIRA PERGUNTA
-  function init() {
-    // Criar a primeira pergunta Aleatoria
-    createQuestion(getRandomInt(0, questionsJs.length));
-  }
-  //Função para gerar numero aleatorio
-  function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
-  }
 
-  // cria uma pergunta
-  function createQuestion(i) {
-    //Limpar a questão Anterior
-    const oldButtons = answersBox.querySelectorAll("button");
-    oldButtons.forEach(function (btn) {
-      btn.remove();
-    });
-    //Alterar o texto da pergunta
-    const questionText = question.querySelector("#question-text");
-    questionText.textContent = questionsJs[i].question;
-    //   Insere as Alternativas
-    questionsJs[i].answers.forEach(function (answer, i) {
-      //Cria o template do botão do quizz
-      const answerTemplate = document
-        .querySelector(".answer-template")
-        .cloneNode(true);
-      const letterBtn = answerTemplate.querySelector(".btn-letter");
-      const answerText = answerTemplate.querySelector(".question-answer");
-      letterBtn.textContent = letters[i];
-      answerText.textContent = answer["answer"];
-      answerTemplate.setAttribute("correct-answer", answer["correct"]);
-
-      //Remover Hide e Temlate Class
-      answerTemplate.classList.remove("hide");
-      answerTemplate.classList.remove("answer-template");
-
-      //Inserir a Alternativa na tela
-      answersBox.appendChild(answerTemplate);
-
-      //Inserir evendo de click no botão
-      answerTemplate.addEventListener("click", function () {
-        checkAnswer(this);
-      });
-    });
-
-    //Incrementar o numero da questão
-    actualQuestions++;
-  }
-
-  //Verificando resposta do usuario
-  function checkAnswer(btn) {
-    // Seleciona todos os botoes
-    const buttons = answersBox.querySelectorAll("button");
-    //verifica se a resposta esta correta e adiciona classes nos botoes
-    buttons.forEach(function (button) {
-      if (button.getAttribute("correct-answer") === "true") {
-        button.classList.add("correct-answer");
-        //checa se o usuario acertou a pergunta
-        if (btn == button) {
-          //incremento dos pontos
-          points++;
-        }
-      } else {
-        button.classList.add("wrong-answer");
-      }
-    });
-
-    //Exibir proxima pergunta
-    nextQuestion();
-  }
-  //Exibe prxima pergunt do quizz
-  function nextQuestion() {
-    //Timer para usuario ver as respostas
-    setTimeout(function () {
-      //Verifica se ainda há perguntas
-      if (actualQuestions >= 10) {
-        //apresenta a mensagem de sucesso
-        showSuccessMessage();
-        return;
-      }
-      //Deixar as questões Aleatorias nas proximas perguntas
-      createQuestion(getRandomInt(0, questionsJs.length));
-    }, 1500);
-  }
-
-  //Exibe a tela final
-  function showSuccessMessage() {
-    hideOrShowQuizz();
-    //TROCAR DADOS DA TABELA DE SUCESSO
-    //   Calcular o score
-    const score = ((points / 10) * 100).toFixed(2);
-    const displayScore = document.querySelector("#display-score span");
-    displayScore.textContent = score.toString();
-    //Alterar o numero de perguntas corretas
-    const correctAnswers = document.querySelector("#correct-answers");
-    correctAnswers.textContent = points;
-    //Alterar o total de perguntas
-    const totalQuestions = document.querySelector("#questions-qty");
-    totalQuestions.textContent = 10;
-  }
-
-  //Mostra ou esconde o score
-  function hideOrShowQuizz() {
-    quizzContainer.classList.toggle("hide");
-    scoreContainer.classList.toggle("hide");
-  }
-
-  // Reiniciar Quizz
-  const restartBtn = document.querySelector("#restart");
-  restartBtn.addEventListener("click", function () {
-    //zerar o jogo
-    actualQuestions = 0;
-    points = 0;
-    hideOrShowQuizz();
-    init();
-  });
-
-  //Inicialização do Quizz
-  init();
-}
+  //INICIO DO CODIGO DE JAVA SCRIPT
+} 
