50052162a0eb274d54915fa51b5813bc27a1dd81
Marcio Paranhos <marcio.naga@gmail.com>
2022-09-07 18:19:57 -0300

    Inicio das perguntas sobre JS

--- js/scripts.js
+++ js/scripts.js
@@ -30,6 +30,8 @@ if (data.tech === "php") {
   // PERGUNTAS
   // SUBISTITUIÇÂO DO QUIZZ PARA A PRIMEIRA PERGUNTA
   function init() {
+    const languageTitle = document.querySelector("#language-title");
+    languageTitle.textContent = "PHP"
     //Zera o array de consulta de numero aleatorio ao iniciar o jogo novamente
     numberNoRepeat = [];
     // Criar a primeira pergunta Aleatoria
@@ -128,6 +130,153 @@ if (data.tech === "php") {
       }
       //Deixar as questões Aleatorias nas proximas perguntas
       createQuestion(getRandomInt(0, questionsPhp.length));
+    }, 1500);
+  }
+
+  //Exibe a tela final
+  function showSuccessMessage() {
+    hideOrShowQuizz();
+    //TROCAR DADOS DA TABELA DE SUCESSO
+    //   Calcular o score
+    const score = ((points / 15) * 100).toFixed(2);
+    const displayScore = document.querySelector("#display-score span");
+    displayScore.textContent = score.toString();
+    //Alterar o numero de perguntas corretas
+    const correctAnswers = document.querySelector("#correct-answers");
+    correctAnswers.textContent = points;
+    //Alterar o total de perguntas
+    const totalQuestions = document.querySelector("#questions-qty");
+    totalQuestions.textContent = 15;
+  }
+
+  //Mostra ou esconde o score
+  function hideOrShowQuizz() {
+    quizzContainer.classList.toggle("hide");
+    scoreContainer.classList.toggle("hide");
+  }
+
+  // Reiniciar Quizz
+  const restartBtn = document.querySelector("#restart");
+  restartBtn.addEventListener("click", function () {
+    //zerar o jogo
+    actualQuestions = 0;
+    points = 0;
+    hideOrShowQuizz();
+    init();
+  });
+
+  //Inicialização do Quizz
+  init();
+
+  //INICIO DO CODIGO DE JAVA SCRIPT
+}
+
+if (data.tech === "js") {
+  console.log(questionsJs.length);
+
+  // PERGUNTAS
+  // SUBISTITUIÇÂO DO QUIZZ PARA A PRIMEIRA PERGUNTA
+  function init() {
+    const languageTitle = document.querySelector("#language-title");
+    languageTitle.textContent = "JavaScript"
+    //Zera o array de consulta de numero aleatorio ao iniciar o jogo novamente
+    numberNoRepeat = [];
+    // Criar a primeira pergunta Aleatoria
+    createQuestion(getRandomInt(0, questionsJs.length));
+  }
+  //Função para gerar numero aleatorio
+  function getRandomInt(min, max) {
+    const numberRandom = Math.floor(Math.random() * (max - min)) + min;
+    if (numberNoRepeat.indexOf(numberRandom) === -1) {
+      numberNoRepeat.push(numberRandom);
+      return numberRandom;
+    } else {
+      //Chama uma função pra escolher outro numero aleatorio caso o numero ja foi sorteado
+      nextQuestionRandom();
+    }
+  }
+  function nextQuestionRandom() {
+    //Timer para usuario ver as respostas
+    setTimeout(function () {
+      //Deixar as questões Aleatorias nas proximas perguntas
+      createQuestion(getRandomInt(0, questionsJs.length));
+    }, 0);
+  }
+
+  // cria uma pergunta
+  function createQuestion(i) {
+    //Limpar a questão Anterior
+    const oldButtons = answersBox.querySelectorAll("button");
+    oldButtons.forEach(function (btn) {
+      btn.remove();
+    });
+    //Alterar o texto da pergunta
+    const questionText = question.querySelector("#question-text");
+    const questionNum = questionNumber.querySelector("#select-number");
+    questionText.textContent = questionsJs[i].question;
+    questionNum.textContent = numberNoRepeat.length;
+    //   Insere as Alternativas
+    questionsJs[i].answers.forEach(function (answer, i) {
+      //Cria o template do botão do quizz
+      const answerTemplate = document
+        .querySelector(".answer-template")
+        .cloneNode(true);
+      const letterBtn = answerTemplate.querySelector(".btn-letter");
+      const answerText = answerTemplate.querySelector(".question-answer");
+      letterBtn.textContent = letters[i];
+      answerText.textContent = answer["answer"];
+      answerTemplate.setAttribute("correct-answer", answer["correct"]);
+
+      //Remover Hide e Temlate Class
+      answerTemplate.classList.remove("hide");
+      answerTemplate.classList.remove("answer-template");
+
+      //Inserir a Alternativa na tela
+      answersBox.appendChild(answerTemplate);
+
+      //Inserir evendo de click no botão
+      answerTemplate.addEventListener("click", function () {
+        checkAnswer(this);
+      });
+    });
+
+    //Incrementar o numero da questão
+    actualQuestions++;
+  }
+
+  //Verificando resposta do usuario
+  function checkAnswer(btn) {
+    // Seleciona todos os botoes
+    const buttons = answersBox.querySelectorAll("button");
+    //verifica se a resposta esta correta e adiciona classes nos botoes
+    buttons.forEach(function (button) {
+      if (button.getAttribute("correct-answer") === "true") {
+        button.classList.add("correct-answer");
+        //checa se o usuario acertou a pergunta
+        if (btn == button) {
+          //incremento dos pontos
+          points++;
+        }
+      } else {
+        button.classList.add("wrong-answer");
+      }
+    });
+
+    //Exibir proxima pergunta
+    nextQuestion();
+  }
+  //Exibe prxima pergunt do quizz
+  function nextQuestion() {
+    //Timer para usuario ver as respostas
+    setTimeout(function () {
+      //Verifica se ainda há perguntas
+      if (actualQuestions >= 15) {
+        //apresenta a mensagem de sucesso
+        showSuccessMessage();
+        return;
+      }
+      //Deixar as questões Aleatorias nas proximas perguntas
+      createQuestion(getRandomInt(0, questionsJs.length));
     }, 1500);
   }
 
--- modules/questionsJs.js
+++ modules/questionsJs.js
@@ -1,131 +1,68 @@
-const questions = [
+const questionsJs = [
     {
       question: "JavaScript foi desenvolvido para qual fim?",
       answers: [
-        { answer: "Back-end", correct: true },
+        { answer: "Back-end", correct: false },
         { answer: "Front-end", correct: false },
-        { answer: "Sistema Operacional", correct: false },
+        { answer: "Full Stack", correct: true },
         { answer: "Banco de dados", correct: false },
       ],
     },
     {
-      question: "Quando o PHP foi criado?",
+      question: "Qual o verdadeiro nome do JavaScript?",
       answers: [
-        { answer: "2000", correct: false },
-        { answer: "1990", correct: false },
-        { answer: "1995", correct: false },
-        { answer: "1994", correct: true },
+        { answer: "ESScript", correct: false },
+        { answer: "TypeScript", correct: false },
+        { answer: "ErroundScript", correct: false },
+        { answer: "ECMAScript", correct: true },
       ],
     },
     {
-      question: "Como declarar uma variavel em PHP?",
+      question: "Como declarar uma variavel em JavaScript?",
       answers: [
-        { answer: "$variavel", correct: true },
-        { answer: "&variavel", correct: false },
-        { answer: "#variavel", correct: false },
-        { answer: "@variavel", correct: false },
+        { answer: "Const variavel", correct: true },
+        { answer: "$variavel", correct: false },
+        { answer: "Cosnt $variavel", correct: false },
+        { answer: "Cosnt_variavel", correct: false },
       ],
     },
     {
-      question: "Como importar um arquivo externo com PHP?",
+      question: "Quando foi criado o JavaScript?",
       answers: [
-        { answer: "require_import", correct: false },
-        { answer: "import_", correct: false },
-        { answer: "@import", correct: false },
-        { answer: "require_once", correct: true },
+        { answer: "1998", correct: false },
+        { answer: "1995", correct: true },
+        { answer: "1994", correct: false },
+        { answer: "1992", correct: false },
       ],
     },
     {
-      question: "Qual é a tag mais comum para inserir PHP em HTML??",
+      question: "Qual seria o resultado de 3 + 2 + ”7 ″?",
       answers: [
-        { answer: "<?=> ……… ?>", correct: false },
-        { answer: "<?php ……… ?>", correct: true },
-        { answer: "<php ……… ?>", correct: false },
-        { answer: "<?php ……… ?php>", correct: false },
+        { answer: "5", correct: false },
+        { answer: "12", correct: false },
+        { answer: "57", correct: true },
+        { answer: "Undefined", correct: false },
       ],
     },
     {
-      question: "Qual era o antigo nome do PHP?",
+      question: "Qual empresa desenvolveu JavaScript?",
       answers: [
-        { answer: "Principal Home Page", correct: false },
-        { answer: "Personal Hyper Page", correct: false },
-        { answer: "Personal Home Page", correct: true },
-        { answer: "Principal Hyper Page", correct: false },
+        { answer: "Microsoft", correct: false },
+        { answer: "Netscape", correct: true },
+        { answer: "Apple", correct: false },
+        { answer: "IBM", correct: false },
       ],
     },
     {
-      question: "Como exibir texto com um script PHP?",
+      question: "Quem criou o JavaScript?",
       answers: [
-        { answer: "print_f", correct: false },
-        { answer: "echo e print", correct: true },
-        { answer: "system_print", correct: false },
-        { answer: "echo e print_f", correct: false },
-      ],
-    },
-    {
-      question: "Em qual versão do PHP foi adicionado o POO?",
-      answers: [
-        { answer: "5", correct: true },
-        { answer: "4", correct: false },
-        { answer: "6", correct: false },
-        { answer: "7", correct: false },
-      ],
-    },
-    {
-      question: "Qual dos frameworks abaixo e de PHP?",
-      answers: [
-        { answer: "React", correct: false },
-        { answer: "CodeIgniter", correct: true },
-        { answer: "Angular", correct: false },
-        { answer: "Express", correct: false },
-      ],
-    },
-    {
-      question: "Como você define uma constante em PHP?",
-      answers: [
-        { answer: "deferer()", correct: false },
-        { answer: "definer()", correct: false },
-        { answer: "defer()", correct: false },
-        { answer: "define()", correct: true },
-      ],
-    },
-    {
-      question: "Como é chamado o PHP?",
-      answers: [
-        { answer: "Hypertext Processor", correct: false },
-        { answer: "Hypertext Preprocessor", correct: true },
-        { answer: "Page Home Processor", correct: false },
-        { answer: "Personal Hyper Parser", correct: false },
-      ],
-    },
-    {
-      question: "Quem conhecemos como o pai do PHP?",
-      answers: [
-        { answer: "Dimitri Lesnter", correct: false },
-        { answer: "Hugo Noyer", correct: false },
-        { answer: "Rasmus Lerdorf", correct: true },
-        { answer: "Leonard Listern", correct: false },
-      ],
-    },
-    {
-      question: "O que queremos dizer quando mencionamos MVC?",
-      answers: [
-        { answer: "Model View Controler", correct: true },
-        { answer: "Model View Container", correct: false },
-        { answer: "Moviment View Conect", correct: false },
-        { answer: "Model Version Controler", correct: false },
-      ],
-    },
-    {
-      question: "O que é necessário para usar a função de imagem??",
-      answers: [
-        { answer: "A biblioteca BM", correct: false },
-        { answer: "A biblioteca GD", correct: true },
-        { answer: "A biblioteca XT", correct: false },
-        { answer: "A biblioteca HR", correct: false },
+        { answer: "Erick Blasmer", correct: false },
+        { answer: "Jonny Hendon", correct: false },
+        { answer: "Brenden Eich", correct: true },
+        { answer: "Robert James", correct: false },
       ],
     },
   ];
   
-  export default questions;
+  export default questionsJs;
   
